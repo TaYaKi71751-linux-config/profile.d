64ebc527b52409b3ff6a2938140a9dad6d42a6e1
TaYaKi71751 <shantian.tw@gmail.com>
2024-02-09 23:35:18 +0900

    Add rm before install

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ FILES=`find sh -type f -name "*.sh"`
 
 for FILE in ${FILES[@]};do
 	FILE_PATH="${PWD}/${FILE}"
+	sudo rm /etc/profile.d/"$(echo $FILE | rev | cut -f1 -d '/' | rev | tr -d '\n' | tr -d '\r')"
 	sudo install "${FILE_PATH}" /etc/profile.d/
 done
